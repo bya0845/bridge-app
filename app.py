354630c94126979ace1e57ec37b274e16bce9364
Yang, Bo Lun <byang0845@gmail.com>
2025-12-17 05:04:04 -0500

    added deployment configs

--- app.py
+++ app.py
@@ -961,5 +961,5 @@ def internal_error(e):
 # ============================================================================
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5001))
+    port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port, debug=True)
